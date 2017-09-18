2bb1568aef5f7552faaec46b1c24a69a8781a07b
Sarah Jad <sarahjad94@gmail.com>
2017-09-18 15:29:59 +0200

    Update user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,24 @@ class UsersController < ApplicationController
     
     end
 
+
+    def edit 
+
+        @user = User.find(params[:id])
+
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was updates Successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+
+    end
+
     private
     def user_params
 

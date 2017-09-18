5249561c127ba1ae14c773be4e1e49377a928e38
Sarah Jad <sarahjad94@gmail.com>
2017-09-18 15:13:57 +0200

    Signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'pages#homepage'
+  
   get 'about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+  
   resources :articles
+  resources :users, except: [:new]
 end
--- db/migrate/20170918123352_add_password_to_users.rb
+++ db/migrate/20170918123352_add_password_to_users.rb
@@ -2,4 +2,4 @@ class AddPasswordToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :password_digest, :string
   end
-end
+end 

2dc9c95183d3a8ea5ef825dbd5449cec32af95bf
Sarah Jad <sarahjad94@gmail.com>
2017-09-18 17:40:09 +0200

    add login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
   resources :articles
   resources :users, except: [:new]
 end

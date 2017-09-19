7cf9944122077c9fe2b1b85dc2b882a31e8bed1d
Sarah Jad <sarahjad94@gmail.com>
2017-09-19 13:04:03 +0200

    Add Admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -80,7 +80,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def require_same_user
 
-        if current_user!= @article.user
+        if current_user!= @article.user and !current_user.admin?
             flash[:danger] = "You can only edit or delete your articles"
             redirect_to root_path
         end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:edit, :update, :show]
-    before_action :require_same_user, only: [:edit, :update]
-
+    before_action :set_user, only: [:edit, :update, :show, :destroy]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+    before_action :require_admin, only: [:destroy]
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
     end
@@ -48,6 +48,14 @@ class UsersController < ApplicationController
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
     end
 
+
+    def destroy
+
+        @user.destroy
+        flash[:danger] = "User and all articles created by user have been deleted"
+        redirect_to users_path
+    end
+
     private
     def user_params
 
@@ -59,12 +67,18 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-        if logged_in? && current_user!= @user
+        if logged_in? && current_user!= @user && !current_user.admin?
             flash[:danger] = "You can only edit your own account"
             redirect_to root_path
         end
     end
 
+    def require_admin
 
+        if logged_in? && !current_user.admin?
+            flash[:danger] = "Onlu Admin users can perform that"
+            redirect_to root_path
+        end
+    end
 
 end
